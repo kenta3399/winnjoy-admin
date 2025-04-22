c96730b8bdd2a8762dbd2c79407ea30582e04e07
Kenta <jodigame4@gmail.com>
2025-04-22 07:45:29 +0700

    🔒 ใช้ .env.local สำหรับ firebase config

--- pages/index.js
+++ pages/index.js
@@ -15,13 +15,13 @@ import {
 
 // ✅ firebaseConfig ที่ใช้งานจริงของเคนตะ
 const firebaseConfig = {
-  apiKey: "AIzaSyBBYFOEqBHaZalIxz1x6DvzjBN1IJjFYnM",
-  authDomain: "winnjoy-admin.firebaseapp.com",
-  projectId: "winnjoy-admin",
-  storageBucket: "winnjoy-admin.appspot.com", // ← ตรงนี้!!
-  messagingSenderId: "999732827140",
-  appId: "1:999732827140:web:83693c5f373bb790d2d151",
-  measurementId: "G-18363QJLX1"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
